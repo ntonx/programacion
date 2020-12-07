a1a6a97dbc4c2b2e71cac5585d5ef44862ee2cf5
Antonio Nicolas ideapad <ntonx9@gmail.com>
2020-12-06 19:05:55 -0600

    add Gato class

--- dev1.py
+++ dev1.py
@@ -1,3 +1,27 @@
 print("dev1.py")
 print("hello world")
 print("this is a test")
+print("new line on py file")
+
+class Gato(object):
+	
+	def __init__(self, nombre="ANONIMO", color="Neutro"):
+		self.nombre=nombre
+		self.color=color
+
+	def ronronear(self):
+		print(self.nombre+" "+"RNRNRNRNRNRNRNRNR")
+
+
+
+
+class GatoSalvaje(Gato):
+	def __init__(self, nombre="ANONIMO", color="Neutro", habitat="selva"):
+		self.habitat=habitat
+		super().__init__(nombre, color)
+	
+	def cazar(self):
+	   	print(self.nombre+" "+"buscando y acechando una presa en "+self.habitat)
+
+g=Gato()
+g.ronronear()
